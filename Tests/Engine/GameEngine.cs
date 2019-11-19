7c4190cc0dad7394ccd48640f771d874ad58f12a
Mantvydas Donauskas <mantvydas.donauskas@outlook.com>
2019-11-19 22:06:05 +0200

    Add tests project

---
+++ Tests/Engine/GameEngine.cs
@@ -0,0 +1,15 @@
+
+using System;
+using Xunit;
+
+namespace Maze.Tests.Engine
+{
+    public class GameEngineTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
